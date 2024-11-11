091ed350cd8cfcb730d42001baadee02f82d5cbb
Bárbara Kógus <barbarakogus@gmail.com>
2024-11-11 16:31:33 +0100

    fix the fetch function

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,23 @@ import { SourceDependencies } from "@/lib/types";
 import { useEffect, useState } from "react";
 
 export default function Home() {
-
   const [sourceDeps, setSourceDeps] = useState<SourceDependencies[]>([]);
 
   useEffect(() => {
     const fetchData = async () => {
-      const response = await fetch('/api/dependencies');
-      const result = await response.json();
-      setSourceDeps(result);
+      try {
+        const response = await fetch("/api/dependencies");
+
+        if (!response.ok) {
+          console.log("res status: ", response.status);
+          throw new Error(`Response status: ${response.status}`);
+        }
+        const result = await response.json();
+        setSourceDeps(result);
+      } catch (err) {
+        console.log(err);
+        throw err;
+      }
     };
 
     fetchData();
@@ -19,27 +28,29 @@ export default function Home() {
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      {sourceDeps && sourceDeps.map((sourceDep, key) => {
-        return (
-        <div key={key}>
-          <p>Source URL: {sourceDep.source.url}</p>
-          <p>Repository Name: {sourceDep.source.repository_name}</p>
-          <p>Repository Owner: {sourceDep.source.repository_owner}</p>
-          <p>Repository Path: {sourceDep.source.path_with_namespace}</p>
-          <br/>
-          <div>
-            {sourceDep.dependencies.map((dep, key) => {
-              return (
-                <div key={key}>
-                  <p>Who: {dep.who}</p>
-                  <p>What: {dep.what}</p>
-                  <p>Value: {dep.value}</p>
-                </div>
-              )
-            })}
-          </div>
-        </div>)
-      })}
+      {sourceDeps &&
+        sourceDeps.map((sourceDep, key) => {
+          return (
+            <div key={key}>
+              <p>Source URL: {sourceDep.source.url}</p>
+              <p>Repository Name: {sourceDep.source.repository_name}</p>
+              <p>Repository Owner: {sourceDep.source.repository_owner}</p>
+              <p>Repository Path: {sourceDep.source.path_with_namespace}</p>
+              <br />
+              <div>
+                {sourceDep.dependencies.map((dep, key) => {
+                  return (
+                    <div key={key}>
+                      <p>Who: {dep.who}</p>
+                      <p>What: {dep.what}</p>
+                      <p>Value: {dep.value}</p>
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+          );
+        })}
     </div>
   );
 }
