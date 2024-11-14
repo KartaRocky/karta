d97401241f57192a7d67134613a1b7d3725e5452
Thiago Diniz da Silveira <thiagods.ti@gmail.com>
2024-11-14 11:55:02 +0100

    Add new endpoint to find dependencies by source

---
+++ app/api/dependencies/[source]/route.ts
@@ -0,0 +1,13 @@
+// app/api/sources/route.ts
+import { NextResponse } from 'next/server';
+import { findAllSourceDependenciesBySource } from '@/lib/sources/sourceRepository';
+
+export async function GET(req: Request,
+  { params }: { params: Promise<{ source: string }> }
+) {
+  const source = (await params).source
+  console.log('source', source)
+  const deps = await findAllSourceDependenciesBySource(source)
+  console.log('deps', deps)
+  return NextResponse.json(deps, { status: 200 });
+}
